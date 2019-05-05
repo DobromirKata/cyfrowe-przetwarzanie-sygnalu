6fe81c84bda911f7c2f7b8995e410a24e796592f
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-05 16:34:20 +0200

    Dodanie interfejsu

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -5,6 +5,23 @@ import javax.swing.*;
 public class View {
     private JFrame frame;
     private JPanel mainPanel;
+    private JComboBox wyborSygnalu;
+    private JSpinner poleAmplituda;
+    private JSpinner poleCzasPocz;
+    private JSpinner poleCzasSyg;
+    private JSpinner poleWspWyp;
+    private JSpinner poleOkres;
+    private JSpinner poleCzasSkoku;
+    private JSpinner poleProbkaSkoku;
+    private JSpinner poleIloscPr;
+    private JSpinner poleCzestotliwosc;
+    private JSlider slider1;
+    private JButton generujButton;
+    private JButton wczytajButton;
+    private JButton wyświetlButton;
+    private JButton zapiszButton;
+    private JList list1;
+    private JLabel histoNum;
 
     public View() {
         // TODO: Tytuł okna programu
@@ -12,7 +29,7 @@ public class View {
 
         frame.setContentPane(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900, 600);
+        frame.setSize(900, 700);
         frame.setVisible(true);
     }
 }
