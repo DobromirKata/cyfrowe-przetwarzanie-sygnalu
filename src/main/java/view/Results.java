d86df4deb2ff6c6669f65ddb0d45ee755b0633cc
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-05 20:03:53 +0200

    Dodanie okna wynikowego

---
+++ src/main/java/view/Results.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class Results {
+    private JPanel mainResultsPanel;
+    private JPanel sygnalPanel;
+    private JPanel histogramPanel;
+    private JPanel danePanel;
+}
